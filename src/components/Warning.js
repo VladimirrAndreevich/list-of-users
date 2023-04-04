83dde9a3083d2fab32ba2640e9bb48917260a026
Vladimir <ryzhovvladimirr@gmail.com>
2023-04-04 08:33:19 +0300

    add portal to warning

--- src/components/Warning.js
+++ src/components/Warning.js
@@ -1,3 +1,17 @@
+import ReactDOM from "react-dom";
+
+const ModalOverlay = (props) => {
+	return (
+		<div className="warning__outer" onClick={props.onClickHandler}>
+			<div className="warning">
+				<h2 className="warning__title">{props.title}</h2>
+				<div className="warning__description">{props.description}</div>
+				<button className="warning__btn">Okay</button>
+			</div>
+		</div>
+	);
+};
+
 function Warning(props) {
 	const onClickHandler = (event) => {
 		if (
@@ -8,14 +22,14 @@ function Warning(props) {
 		}
 	};
 
-	return (
-		<div className="warning__outer" onClick={onClickHandler}>
-			<div className="warning">
-				<h2 className="warning__title">{props.title}</h2>
-				<div className="warning__description">{props.description}</div>
-				<button className="warning__btn">Okay</button>
-			</div>
-		</div>
+	return ReactDOM.createPortal(
+		<ModalOverlay
+			onClickHandler={onClickHandler}
+			title={props.title}
+			description={props.description}
+			onClose={props.onClose}
+		/>,
+		document.getElementById("modal-root")
 	);
 }
 
